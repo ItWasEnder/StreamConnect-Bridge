58436d9e1927c52fd7e7c76f874689faf238e38a
EnderGamingFilms <endergamingfilms@gmail.com>
2024-02-20 03:21:42 -0800

    cleanup jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,3 @@
-import type { Config } from 'jest';
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
@@ -9,42 +8,7 @@ const jestConfig: JestConfigWithTsJest = {
 	collectCoverageFrom: ['src/**/*.{ts,tsx}', 'src/**/*.{js,jsx}', '!**/node_modules/**'],
 	maxConcurrency: 5,
 	preset: 'ts-jest',
-	testPathIgnorePatterns: ['<rootDir>/src/__tests__/data/']
-	// transform: {
-	// 	'^.+\\.m?[tj]sx?$': [
-	// 		'ts-jest',
-	// 		{
-	// 			useESM: true
-	// 		}
-	// 	]
-	// },
-	// moduleNameMapper: {
-	// 	'(.+)\\.js': '$1'
-	// },
-	// extensionsToTreatAsEsm: ['.ts']
+	testMatch: ['**/?(*.)+(test).ts']
 };
 
 export default jestConfig;
-
-// export default async (): Promise<Config> => {
-// 	return {
-// 		automock: true,
-// 		verbose: true,
-// 		collectCoverage: true,
-// 		collectCoverageFrom: ['src/**/*.{ts,tsx}', 'src/**/*.{js,jsx}', '!**/node_modules/**'],
-// 		maxConcurrency: 5,
-// 		preset: 'ts-jest',
-// 		// transform: {
-// 		// 	'\\.[jt]sx?$': [
-// 		// 		'ts-jest',
-// 		// 		{
-// 		// 			useESM: true
-// 		// 		}
-// 		// 	]
-// 		// },
-// 		// moduleNameMapper: {
-// 		// 	'(.+)\\.js': '$1'
-// 		// },
-// 		// extensionsToTreatAsEsm: ['.ts']
-// 	};
-// };
