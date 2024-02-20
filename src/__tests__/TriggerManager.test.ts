fe14ba2bdd0cb0285b96711db5e09484fd9f9835
EnderGamingFilms <endergamingfilms@gmail.com>
2024-02-19 19:37:30 -0800

    fixed trigger test

--- src/__tests__/TriggerManager.test.ts
+++ src/__tests__/TriggerManager.test.ts
@@ -1,12 +1,16 @@
 import { expect, jest, test } from '@jest/globals';
-import { ActionsManager } from '../actions/ActionsManager';
+import { TriggerManager } from '../triggers/TriggerManager';
+import { FileManager } from '../utils/FileManager';
 
 // import * as ActionsManager from '../actions/ActionsManager';
 // const { getKeys } = jest.requireActual<typeof ActionsManager>('../actions/ActionsManager.ts');
 
+jest.mock('../utils/FileManager')
+const _fileManager = <jest.Mocked<FileManager>><unknown>FileManager;
+
 describe('ActionsManager', () => {
 	test('getKeys', () => {
-		const actionsManager = new ActionsManager();
+		const triggerManager = new TriggerManager(_fileManager);
 
 		// const actionsManager = new ActionsManager();
 		// expect(actionsManager.getKeys()).toEqual(['key1', 'key2']);
